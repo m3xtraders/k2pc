064c5b49ef7269dd19a1139602c2b4058fb341d3
Hannan Khan <122686881+Hannankhan00@users.noreply.github.com>
2026-09-05 15:59:46 +0500

    redesign of mobile vi

--- components/sections/TrustGuaranteeStrip.tsx
+++ components/sections/TrustGuaranteeStrip.tsx
@@ -62,24 +62,15 @@ const TRUST_ITEMS: TrustItem[] = [
     title: "CANADIAN OWNED & OPERATED",
     subtitle: "Proudly Saskatchewan-founded in Saskatoon",
     renderIcon: () => (
-      /* Official National Flag of Canada */
+      /* Official National Flag of Canada (Pantone Standards) */
       <svg
-        className="w-14 h-10 sm:w-16 sm:h-11 lg:w-20 lg:h-14 transition-transform duration-300 group-hover:scale-110 drop-shadow-md rounded-lg overflow-hidden border border-stone-200/90 shrink-0"
-        viewBox="0 0 1000 500"
+        className="w-16 h-10 sm:w-18 sm:h-11 lg:w-20 lg:h-12 transition-transform duration-300 group-hover:scale-110 drop-shadow-md rounded-md overflow-hidden border border-stone-200/90 shrink-0"
         xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 9600 4800"
         aria-hidden="true"
       >
-        {/* Left Red Band */}
-        <rect width="250" height="500" fill="#E00613" />
-        {/* Central White Square */}
-        <rect x="250" width="500" height="500" fill="#FFFFFF" />
-        {/* Right Red Band */}
-        <rect x="750" width="250" height="500" fill="#E00613" />
-        {/* Official Stylized 11-Point Canadian Maple Leaf */}
-        <path
-          d="M500,430 L488,430 L485,348 L418,375 L432,328 L370,332 L398,284 L335,268 L348,242 L262,228 L308,188 L278,168 L368,145 L360,118 L432,142 L455,80 L488,110 L500,60 L512,110 L545,80 L568,142 L640,118 L632,145 L722,168 L692,188 L738,228 L652,242 L665,268 L602,284 L630,332 L568,328 L582,375 L515,348 L512,430 Z"
-          fill="#E00613"
-        />
+        <path fill="#d52b1e" d="m0 0h2400l99 99h4602l99-99h2400v4800h-2400l-99-99h-4602l-99 99H0z" />
+        <path fill="#fff" d="m2400 0h4800v4800h-4800zm2490 4430-45-863a95 95 0 0 1 111-98l859 151-116-320a65 65 0 0 1 20-73l941-762-212-99a65 65 0 0 1-34-79l186-572-542 115a65 65 0 0 1-73-38l-105-247-423 454a65 65 0 0 1-111-57l204-1052-327 189a65 65 0 0 1-91-27l-332-652-332 652a65 65 0 0 1-91 27l-327-189 204 1052a65 65 0 0 1-111 57l-423-454-105 247a65 65 0 0 1-73 38l-542-115 186 572a65 65 0 0 1-34 79l-212 99 941 762a65 65 0 0 1 20 73l-116 320 859-151a95 95 0 0 1 111 98l-45 863z" />
       </svg>
     ),
   },
